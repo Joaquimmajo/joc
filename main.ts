f130093fc541428238f6c4176e8f20d25d5ca3f4
Joaquimmajo <82875861+Joaquimmajo@users.noreply.github.com>
2021-04-30 14:03:38 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,20 @@
-
+input.onButtonPressed(Button.A, function () {
+    if (spirite.get(LedSpriteProperty.X) == 2) {
+        game.addScore(1)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    game.pause()
+    basic.showNumber(game.score())
+    basic.pause(1000)
+    game.resume()
+})
+let spirite: game.LedSprite = null
+spirite = game.createSprite(2, 2)
+basic.forever(function () {
+    spirite.move(1)
+    basic.pause(100)
+    if (spirite.isTouchingEdge()) {
+        spirite.ifOnEdgeBounce()
+    }
+})
